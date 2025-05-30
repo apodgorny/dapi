5e0b6f2c965611187bd134d561a870d257fa2453
Alexander Podgorny <alexanderpodgorny@gmail.com>
2025-05-30 04:54:59 +0300

    Stable db saving/retrieval of O

--- lib/edge.py
+++ lib/edge.py
@@ -6,7 +6,7 @@ class Edge:
 	@property
 	def model(self):
 		return EdgeRecord
-		
+
 	def __init__(self, session):
 		self.session = session
 
@@ -17,11 +17,11 @@ class Edge:
 		self.session.commit()
 		return edge
 
-	def remove_for(self, obj: Any):
-		self.session.query(EdgeRecord).filter(
-			(EdgeRecord.id1 == obj.id) | (EdgeRecord.id2 == obj.id)
-		).delete()
-		self.session.commit()
+	# def remove_for(self, obj: Any):
+	# 	self.session.query(EdgeRecord).filter(
+	# 		(EdgeRecord.id1 == obj.id) | (EdgeRecord.id2 == obj.id)
+	# 	).delete()
+	# 	self.session.commit()
 
 	def get_edges(self, obj: Any, rel: str = None):
 		query = self.session.query(EdgeRecord).filter(
@@ -33,18 +33,18 @@ class Edge:
 			)
 		return query.all()
 
-	def replace_all(self, obj: Any, rel: str, new_ids: list[int], type2: str):
-		self.session.query(EdgeRecord).filter(
-			EdgeRecord.id1  == obj.id,
-			EdgeRecord.rel1 == rel
-		).delete()
-		for index, id2 in enumerate(new_ids):
-			self.create(
-				id1   = obj.id,
-				type1 = obj.__class__.__name__,
-				id2   = id2,
-				type2 = type2,
-				rel1  = rel,
-				key1  = str(index),
-			)
-		self.session.commit()
+	# def replace_all(self, obj: Any, rel: str, new_ids: list[int], type2: str):
+	# 	self.session.query(EdgeRecord).filter(
+	# 		EdgeRecord.id1  == obj.id,
+	# 		EdgeRecord.rel1 == rel
+	# 	).delete()
+	# 	for index, id2 in enumerate(new_ids):
+	# 		self.create(
+	# 			id1   = obj.id,
+	# 			type1 = obj.__class__.__name__,
+	# 			id2   = id2,
+	# 			type2 = type2,
+	# 			rel1  = rel,
+	# 			key1  = str(index),
+	# 		)
+	# 	self.session.commit()
