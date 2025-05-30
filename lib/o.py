a275e4695ed4061ee828c6d38b4294b9fce4d824
Alexander Podgorny <alexanderpodgorny@gmail.com>
2025-05-30 04:54:46 +0300

    Stable db saving/retrieval of O

--- lib/o.py
+++ lib/o.py
@@ -9,26 +9,6 @@ from .odb       import ODB
 
 class O(BaseModel):
 
-	@classmethod
-	def _make_optional(cls, tp):
-		origin = get_origin(tp)
-		args   = get_args(tp)
-
-		if cls.is_o_type(tp) or (
-			origin in (list, List, dict, Dict)
-			and any(cls.is_o_type(a) for a in args)
-		):
-			return Optional[tp]
-		return tp
-
-	def __init_subclass__(cls, **kwargs):
-		super().__init_subclass__(**kwargs)
-		for name, field in cls.model_fields.items():
-			new_type = cls._make_optional(field.annotation)
-			if new_type != field.annotation:
-				field.annotation = new_type
-				field.required   = False
-
 	# Magic
 	############################################################################
 
@@ -57,38 +37,38 @@ class O(BaseModel):
 	# Public class methods
 	############################################################################
 
-	@classmethod
-	def is_allowed_type(cls, tp: Any) -> bool:
-		origin = get_origin(tp)
-		args   = get_args(tp)
+	# @classmethod
+	# def is_allowed_type(cls, tp: Any) -> bool:
+	# 	origin = get_origin(tp)
+	# 	args   = get_args(tp)
 
-		# Allow Optional[T]
-		if origin is Union and type(None) in args:
-			return cls.is_allowed_type(args[0])
+	# 	# Allow Optional[T]
+	# 	if origin is Union and type(None) in args:
+	# 		return cls.is_allowed_type(args[0])
 
-		# Allow primitives
-		if tp in (str, int, float, bool, type(None)):
-			return True
+	# 	# Allow primitives
+	# 	if tp in (str, int, float, bool, type(None)):
+	# 		return True
 
-		# Allow List[T]
-		if origin in (list, List):
-			return len(args) == 1 and cls.is_allowed_type(args[0])
+	# 	# Allow List[T]
+	# 	if origin in (list, List):
+	# 		return len(args) == 1 and cls.is_allowed_type(args[0])
 
-		# Allow Dict[str, T]
-		if origin in (dict, Dict):
-			return len(args) == 2 and args[0] is str and cls.is_allowed_type(args[1])
+	# 	# Allow Dict[str, T]
+	# 	if origin in (dict, Dict):
+	# 		return len(args) == 2 and args[0] is str and cls.is_allowed_type(args[1])
 
-		# Allow O models
-		if cls.is_o_type(tp):
-			return True
+	# 	# Allow O models
+	# 	if cls.is_o_type(tp):
+	# 		return True
 
-		return False  # Disallow all else
+	# 	return False  # Disallow all else
 
-	@classmethod
-	def validate_types(cls):
-		for name, field in cls.model_fields.items():
-			if not cls.is_allowed_type(field.annotation):
-				raise TypeError(f'Disallowed type in `{cls.__name__}` field `{name}`: {field.annotation}')
+	# @classmethod
+	# def validate_types(cls):
+	# 	for name, field in cls.model_fields.items():
+	# 		if not cls.is_allowed_type(field.annotation):
+	# 			raise TypeError(f'Disallowed type in `{cls.__name__}` field `{name}`: {field.annotation}')
 
 	@classmethod
 	def Field(cls, *args, description='', semantic=False, **kwargs):
